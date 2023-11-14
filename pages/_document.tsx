85424008c27d3ca615fb23348ba68cabdaf9a5aa
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2023-11-14 13:23:25 +0300

    fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-
+      <Head>
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="preload" href="/fonts/Actay-Regular.otf" as="font" type="font/otf" crossOrigin="" />
+        <link rel="preload" href="/fonts/ActayWide-Bold.otf" as="font" type="font/otf" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-Black.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-Bold.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-ExtraBold.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-ExtraLight.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-Medium.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-Regular.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Onest-Thin.woff2" as="font" type="font/woff2" crossOrigin="" />
+      </Head>
       <body>
         <Main />
         <NextScript />
